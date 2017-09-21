a6ee5d13ad90d1de5e66d778917eb8b42796b2ed
Simo Kinnunen <simo@shoqolate.com>
2017-09-21 23:45:42 +0900

    Got rid of CoffeeScript. Functionally nothing should change - even old bugs (namely with fraction types) are kept.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
